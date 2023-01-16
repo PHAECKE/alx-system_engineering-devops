e663a089401eb83a951a34776310bbcd28148715
PHAECKE <francisamexo@yahoo.com>
2023-01-16 16:48:07 +0000

    ..

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
-tion for a#!/usr/bin/python3
-"""Exports to-do list i given employee ID to CSV format."""
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
-            ) for t in todos]
+         ) for t in todos]
